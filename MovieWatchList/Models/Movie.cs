0b6962380746e07832cbae0bfdbb228d071f807c
Maricar Walters <walterskai7@gmail.com>
2021-02-23 14:30:23 -0600

    Initial commit for mmovie watch listc MVC app

---
+++ MovieWatchList/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieWatchList.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+
+        public Movie()
+        {
+
+        }
+        public Movie(string title, string genre)
+        {
+            Title = title;
+            Genre = genre;
+        }
+    }
+}
---
+++ MovieWatchList/ViewModels/AddMovieViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieWatchList.ViewModels
+{
+    public class AddMovieViewModel
+    {
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
+        public string Genre { get; set; }
+    }
+}
