36874573631e3704ff192e5bac6d3619f09e87ec
Maricar Walters <walterskai7@gmail.com>
2021-02-24 15:12:29 -0600

    Added edit/update function

--- MovieWatchList/Controllers/MovieController.cs
+++ MovieWatchList/Controllers/MovieController.cs
@@ -26,21 +26,19 @@ namespace MovieWatchList.Controllers
         // GET
         public IActionResult Add()
         {
-           
+
             AddMovieViewModel addNewMovie = new AddMovieViewModel();
 
             return View(addNewMovie);
         }
 
-        //POST
         [HttpPost]
-        //[Route("add")]
         public IActionResult Add(AddMovieViewModel addNewMovie)
         {
             if (ModelState.IsValid)
             {
                 Movie newMovie = new Movie(addNewMovie.Title, addNewMovie.Director, addNewMovie.Genre, addNewMovie.Rating, addNewMovie.MainActor);
-        
+
                 context.Movies.Add(newMovie);
                 context.SaveChanges();
 
@@ -49,5 +47,32 @@ namespace MovieWatchList.Controllers
             return View(addNewMovie);
         }
 
-}
+        [HttpGet]
+        [Route("/Movie/Edit/{movieId}")]
+        public IActionResult Edit(int movieId)
+        {
+            Movie movieToBeEdited = context.Movies.Find(movieId);
+            ViewBag.movieEdit = movieToBeEdited;
+            ViewBag.title = movieToBeEdited.Title;
+
+            context.SaveChanges();
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Movie/Edit")]
+        public IActionResult SubmitEditMovieForm(int movieId, string title, string director, string genre, double rating, string mainActor)
+        {
+            Movie toEdit = context.Movies.Find(movieId);
+            toEdit.Title = title;
+            toEdit.Genre = genre;
+            toEdit.Director = director;
+            toEdit.Rating = rating;
+            toEdit.MainActor = mainActor;
+
+            context.SaveChanges();
+
+            return Redirect("Movie/Index");
+        }
+    }
 }
--- MovieWatchList/ViewModels/AddMovieViewModel.cs
+++ MovieWatchList/ViewModels/AddMovieViewModel.cs
@@ -13,12 +13,17 @@ namespace MovieWatchList.ViewModels
         public string Title { get; set; }
 
         [Required(ErrorMessage = "Director is required")]
+        [StringLength(30, ErrorMessage = "Input can not exceed 50 characters")]
         public string Director { get; set; }
 
         [Required(ErrorMessage = "Genre is required")]
+        [StringLength(30, ErrorMessage = "Input can not exceed 50 characters")]
         public string Genre { get; set; }
-    
+
+        [Range(0, 10, ErrorMessage = "Please enter a range between 1-10")]
         public double Rating { get; set; }
+
+        [StringLength(50, ErrorMessage = "Input can not exceed 50 characters")]
         public string MainActor{ get; set; }
         public AddMovieViewModel() { }
         //public AddMovieViewModel(string title, string genre) 
