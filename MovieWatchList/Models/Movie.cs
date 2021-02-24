01cd314598a6f17b46273e819ca8ac1df7ba736c
Maricar Walters <walterskai7@gmail.com>
2021-02-24 13:49:37 -0600

    Update view to show sql server entries

--- MovieWatchList/Models/Movie.cs
+++ MovieWatchList/Models/Movie.cs
@@ -11,6 +11,8 @@ namespace MovieWatchList.Models
         public string Title { get; set; }
         public string Genre { get; set; }
 
+        //public string Director { get; set; }
+
         public Movie()
         {
 
